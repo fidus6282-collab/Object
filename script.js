f9ec1e1f685e14a0bb179e100eed7364d876ee8b
fidus6282-collab <fidus6282@gmail.com>
2025-11-15 00:41:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// let person = {
+//     name: "Aziza",
+//     age: 17,
+//     isStudent: true
+// }
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person["isStudent"]);
+
+// person.country = "Uzbekistan";
+// person["hobby"] = "Coding";
+
+// console.log(person);
+
+// person.age = 18;
+// console.log(person.age);
+
+// delete person.hobby;
+// console.log(person);
+
+let obj = {
+  ism: null,
+  id: null,
+  parol: null
+};
+
+let ism = prompt('Ismingizni kiritnig')
+let id = prompt('id idingizni kiriting ')
+let parol = prompt('parolingizni kiritnig')
+
+console.log(ism);
+console.log(id)
+console.log(parol)
+
+obj.ism = ism
+obj.id = id
+obj.parol = parol
+
+console.log(obj)
+
+
